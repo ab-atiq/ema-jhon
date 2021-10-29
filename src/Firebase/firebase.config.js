87d4b1e8a4ed20ee9b4b805b6892376ce935eda9
ATIQUR RAHMAN <abulbasharatiqur@gmail.com>
2021-10-29 23:48:03 +0600

    ema-jhon

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// console.log(process.env);
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+};
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDy9x9mL0PcOwjvooHH_JChPAjwXaA8MVw",
+//     authDomain: "ema-jhon-authentication-37970.firebaseapp.com",
+//     projectId: "ema-jhon-authentication-37970",
+//     storageBucket: "ema-jhon-authentication-37970.appspot.com",
+//     messagingSenderId: "614952278105",
+//     appId: "1:614952278105:web:0494b2176f557aaed4f948"
+// };
+
+export default firebaseConfig;
